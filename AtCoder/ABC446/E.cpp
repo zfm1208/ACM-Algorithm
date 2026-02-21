5404e77d492141a0068dae5fa40e81d763011f62
zfm1208 <cuikales954@gmail.com>
2026-02-21 19:56:41 +0800

    ABC-446

---
+++ AtCoder/ABC446/E.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i, l, r) for (int i = l; i <= r; i++)
+#define pii pair<int, int>
+#define int long long
+#define pb push_back
+#define fi first
+#define se second
+#define endl '\n'
+double pi = acos(-1);
+const int N = 1e6, mod = 1e9+7, inf = 1e18 + 5;
+int fpow(int a,int b){
+    int res = 1;
+    while(b){
+        if(b&1) res = res * a % mod;
+        a = a * a % mod;
+        b >>= 1;
+    }
+    return res % mod;
+}
+
+void solve(){
+
+}
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int T = 1;
+    //cin >> T;
+    while(T--)
+        solve();
+    return 0;
+}

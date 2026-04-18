ec1fbc0bf8b7c7f944a8a157ed98b4cb22804a41
zfm1208 <cuikales954@gmail.com>
2026-04-18 10:34:39 +0800

    4.18

--- 天梯赛/L2-013 红色警报.cpp
+++ 天梯赛/L2-013 红色警报.cpp
@@ -9,16 +9,67 @@ using namespace std;
 #define endl '\n'
 double pi = acos(-1);
 const int N = 1e6, mod = 1e9+7, inf = 1e18 + 5;
-
+int fa[N];
+bool vis[N];
+struct node {
+    int u,v;
+};
+vector<node> e;
+int n,m;
+int find(int x){
+    return x == fa[x] ? x : fa[x] = find(fa[x]);
+}
+void merge(int x, int y) {
+    int fx = find(x);
+    int fy = find(y);
+    if(fx != fy) {
+        fa[fx] = fy;
+    }
+}
+int count() {
+    for(int i = 0; i < n; i++) fa[i] = i;
+    for(int i = 0; i < e.size(); i++){
+        auto [u,v] = e[i];
+        if(vis[u] && vis[v]) merge(u,v);
+    }
+    int cnt = 0;
+    for(int i = 0; i < n; i++){
+        if(vis[i] && fa[i] == i) cnt++;
+    }
+    return cnt;
+}
 void solve(){
-    
+    cin >> n >> m;
+    rep(i,0,n-1) vis[i] = 1;
+    for(int i = 1; i <= m; i++){
+        int u,v; cin >> u >> v;
+        e.push_back({u,v});
+    }
+    int cnt = count();
+    int k; cin >> k;
+    int zha = 0;
+    while(k--){
+        int ct; cin >> ct;
+        vis[ct] = 0;
+        zha++;
+        int cc = count();
+        if(cc > cnt){
+            cout << "Red Alert: City " << ct << " is lost!\n";
+        } else {
+            cout << "City " << ct << " is lost.\n";
+        }
+        cnt = cc;
+        if(zha == n){
+            cout << "Game Over.\n";
+        }
+    }
 }
 
 signed main(){
     ios::sync_with_stdio(false);
     cin.tie(0);cout.tie(0);
     int T = 1;
-    cin >> T;
+    // cin >> T;
     while(T--)
         solve();
     return 0;

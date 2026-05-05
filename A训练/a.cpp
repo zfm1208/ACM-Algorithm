4d01a8a4b85146392620ddc2ec487adcabe20052
zfm1208 <cuikales954@gmail.com>
2026-05-05 19:20:16 +0800

    5.5

--- A训练/a.cpp
+++ A训练/a.cpp
@@ -11,82 +11,14 @@ double pi = acos(-1);
 const int N = 1e6, mod = 1e9+7, inf = 1e18 + 5;
 
 void solve(){
-    double atk=0;
-    double atkr = 0.0;
-    double cr = 5.0;
-    double cdr = 50.0;
-    for(int i = 1; i <= 25; i++){
-        string s; 
-        getline(cin,s);
-        if(s[0]=='A'&&s[1]=='T'&&s[2]=='K'&&s[3]=='+'){
-            string t;
-            for(int j = 0; j < s.size(); j++){
-                if(s[j] == '+'){
-                    for(int k = j+1; k < s.size(); k++){
-                        t += s[k];
-                    }
-                    break;
-                }
-            }
-            double ans = stod(t);
-            // cout<<t<<'\n';
-            // cout << i << " " << ans << endl;
-            atk += ans;
-        }
-        else if(s.substr(0,8) == "ATK Rate"){
-            string t;
-            for(int j = 0; j < s.size(); j++){
-                if(s[j] == '+'){
-                    for(int k = j+1; k < s.size() - 1; k++){
-                        t += s[k];
-                    }
-                    break;
-                }
-            }
-            double ans = stod(t);
-            atkr += ans;            
-        }
-        else if(s.substr(0,9) == "Crit Rate"){
-            string t;
-            for(int j = 0; j < s.size(); j++){
-                if(s[j] == '+'){
-                    for(int k = j+1; k < s.size() - 1; k++){
-                        t += s[k];
-                    }
-                    break;
-                }
-            }
-            double ans = stod(t);
-            cr += ans;              
-        }
-        else if(s.substr(0,8) == "Crit DMG"){
-            string t;
-            for(int j = 0; j < s.size(); j++){
-                if(s[j] == '+'){
-                    for(int k = j+1; k < s.size() - 1; k++){
-                        t += s[k];
-                    }
-                    break;
-                }
-            }
-            double ans = stod(t);
-            cdr += ans;   
-        }
-    }
-    // cout << atk << " " << atkr << " " << cr << " " << cdr << endl;
-    if(cr >= 100) cr = 100.0;
-    double atk1 = 1500*(1+atkr*1.0/100) + atk;
-    // cout << atk1 << endl;
-    double E;
-    E = atk1 * 1.0 * (1.0 - cr*1.0/100) + atk1 * 1.0 *  (1.0 + cdr *1.0 / 100) * (cr * 1.0 / 100);
-    cout << fixed << setprecision(10) << E << endl;
+    
 }
 
 signed main(){
     ios::sync_with_stdio(false);
     cin.tie(0);cout.tie(0);
     int T = 1;
-    // cin >> T;
+    cin >> T;
     while(T--)
         solve();
     return 0;
--- XCPC/CCPC/2022Nordic/F.Foreign Football.cpp
+++ XCPC/CCPC/2022Nordic/F.Foreign Football.cpp
@@ -10,15 +10,127 @@ using namespace std;
 double pi = acos(-1);
 const int N = 1e6, mod = 1e9+7, inf = 1e18 + 5;
 
+typedef unsigned long long ULL;
+const ULL P = 131;
+
 void solve(){
+    int n; cin >> n;
+    vector<vector<string>> a(n+1,vector<string>(n+1));
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            cin >> a[i][j];
+        }
+    }
+    if (n == 2) {
+        string a12 = a[1][2];
+        string a21 = a[2][1];
+        if(a12.size() != a21.size()){
+            cout << "NONE" << endl;
+            return;
+        }
+        
+        int len = a12.size();
+        
+        vector<ULL> p(len + 1, 1), hA(len + 1, 0), hB(len + 1, 0);
+        for (int i = 1; i <= len; i++) {
+            p[i] = p[i - 1] * P;
+            hA[i] = hA[i - 1] * P + a12[i - 1]; 
+            hB[i] = hB[i - 1] * P + a21[i - 1];
+        }
+        
+        auto get_hash = [&](const vector<ULL>& h, int l, int r) -> ULL {
+            return h[r] - h[l - 1] * p[r - l + 1];
+        };
+
+        int cnt = 0;
+        vector<string> ans(n + 1);
+
+        for (int i = 1; i < len; i++) {
+            ULL hash_s2 = get_hash(hA, i + 1, len);
+            ULL hash_b1 = get_hash(hB, 1, len - i);
+            
+            if (hash_s2 == hash_b1) {
+                ULL hash_s1 = get_hash(hA, 1, i);
+                ULL hash_b_suffix = get_hash(hB, len - i + 1, len);
+                
+                if (hash_s1 == hash_b_suffix) {
+                    if (a12.compare(i, len - i, a21, 0, len - i) == 0 &&
+                        a12.compare(0, i, a21, len - i, i) == 0) {
+                        
+                        cnt++;
+                        if (cnt == 1) { 
+                            ans[1] = a12.substr(0, i);
+                            ans[2] = a12.substr(i);
+                        }
+                        if (cnt == 2) {
+                            break; 
+                        }
+                    }
+                }
+            }
+        }
+        
+        if (cnt == 0) cout << "NONE" << endl;
+        else if (cnt == 1) cout << "UNIQUE\n" << ans[1] << "\n" << ans[2] << endl;
+        else cout << "MANY" << endl; 
+        return;
+    }
+
+    int l12 = a[1][2].size();
+    int l13 = a[1][3].size();
+    int l23 = a[2][3].size();
+
+    if ((l12 + l13 - l23) <= 0 || (l12 + l13 - l23) % 2 != 0) {
+        cout << "NONE" << endl;;
+        return;
+    }
     
+    int len1 = (l12 + l13 - l23) / 2;
+    
+    if (len1 >= l12) {
+        cout << "NONE" << endl;
+        return;
+    }    
+    string s1 = a[1][2].substr(0, len1);
+    vector<string> s(n + 1); 
+    s[1] = s1;
+    for(int i = 2; i <= n; i++){
+        if(a[1][i].size() <= len1){
+            cout << "NONE" << endl;
+            return;
+        }
+        if(a[1][i].compare(0, len1, s1) != 0){
+            cout << "NONE" << endl;
+            return;
+        }
+        s[i] = a[1][i].substr(len1); 
+    }
+
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            if(i == j) continue;
+            if(a[i][j].size() != s[i].size() + s[j].size()){
+                cout << "NONE" << endl;
+                return;
+            }
+            if(a[i][j].compare(0, s[i].size(), s[i]) != 0 || 
+               a[i][j].compare(s[i].size(), s[j].size(), s[j]) != 0) {
+                cout << "NONE" << endl;
+                return;
+            }
+        }
+    }
+    cout << "UNIQUE" << endl;
+    for(int i = 1; i <= n; i++){
+        cout << s[i] << endl;
+    }    
 }
 
 signed main(){
     ios::sync_with_stdio(false);
     cin.tie(0);cout.tie(0);
     int T = 1;
-    cin >> T;
+    // cin >> T;
     while(T--)
         solve();
     return 0;

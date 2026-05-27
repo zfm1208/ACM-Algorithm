019a16aac7bdc43991183044f4dadaa0e343bd91
zfm1208 <cuikales954@gmail.com>
2026-05-27 19:48:34 +0800

    5.27

--- A训练/d.cpp
+++ A训练/d.cpp
@@ -9,90 +9,17 @@ using namespace std;
 #define se second
 #define endl '\n'
 double pi = acos(-1);
-const int N = 1e5, mod = 1e9+7, inf = 1e18 + 5;
-struct BIT {
-    vector<int> tr;
-    int n;
-    BIT(int _n) : n(_n), tr(_n + 1) {}
-    int low_bit(int x) { return x & (-x); }
-    void add(int x, int val) {
-        for(int i = x; i <= n; i += low_bit(i)){
-            tr[i] += val;
-        }
-    }
+const int N = 1e6, mod = 1e9+7, inf = 1e18 + 5;
 
-    int query(int x) {
-        if(x > n) x = n;
-        int res = 0;
-        for(int i = x; i > 0; i -= low_bit(i)) {
-            res += tr[i];
-        }
-        return res;
-    }
-
-    int query(int l, int r) {
-        if(l > r) return 0;
-        return query(r) - query(l - 1);
-    }
-};
-
-struct node {
-    int l, r, k, id;
-};
 void solve(){
-    int n,q; cin >> n >> q;
-    vii a(n + 1), ans(q + 1);
-    rep(i, 1, n) cin >> a[i];
-    int sq = sqrt(N);
-    vector<vector<node>> qs(sq+1);
-    vector<vector<pii>> qs1(n+1);
-    for(int i = 1; i <= q; i++){
-        int l, r, k; 
-        cin >> l >> r >> k;
-        if(k <= sq){
-            qs[k].push_back({l, r, k, i});
-        }else{
-            qs1[r].push_back({k, i});
-            qs1[l-1].push_back({k, -i});
-        }
-    }
-    vii pre(n + 1);
-    for(int k = 1; k <= sq; k++){
-        if(qs[k].empty()) continue;
-        for(int i = 1; i <= n; i++){
-            pre[i] = pre[i-1] + a[i] / k;
-        }
-        for(auto &it: qs[k]){
-            ans[it.id] = pre[it.r] - pre[it.l - 1];
-        }
-    }
-
-    BIT bit(N + 5);
-    for(int i = 1; i <= n; i++) {
-        if(a[i] != 0) bit.add(a[i], 1);
-        for(auto &[k,id] : qs1[i]){
-            int m = N / k;
-            int sum = 0;
-            for(int j = 1; j <= m; j++){
-                int L = j * k;
-                int R = (j + 1) * k - 1;
-                sum += j * (bit.query(L, R));
-            }
-            if(id > 0) ans[id] += sum;
-            else ans[-id] -= sum;
-        }
-    }
-    for(int i = 1; i <= q; i++){
-        cout << ans[i] << endl;
-    }
-
+    
 }
 
 signed main(){
     ios::sync_with_stdio(false);
     cin.tie(0);cout.tie(0);
     int T = 1;
-    // cin >> T;
+    cin >> T;
     while(T--)
         solve();
     return 0;
--- XCPC/ICPC/2026ICPC-河南省赛/J. 神经网络图染.cpp
+++ XCPC/ICPC/2026ICPC-河南省赛/J. 神经网络图染.cpp
@@ -44,3 +44,109 @@ signed main(){
         solve();
     return 0;
 }
+
+
+// n 高达 1e18时可以这样优化
+
+
+
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// #define int long long
+// const int mod = 998244353;
+
+// // 1. 定义 3x3 的矩阵结构体
+// struct Matrix {
+//     int mat[3][3];
+//     // 构造函数：默认初始化为全 0 矩阵
+//     Matrix() {
+//         memset(mat, 0, sizeof(mat));
+//     }
+//     // 重载乘法运算符，实现矩阵乘法 (带取模)
+//     Matrix operator*(const Matrix& other) const {
+//         Matrix res;
+//         for (int i = 0; i < 3; i++) {
+//             for (int k = 0; k < 3; k++) {
+//                 int r = mat[i][k];
+//                 if (r == 0) continue; // 小优化：0 乘任何数都是 0
+//                 for (int j = 0; j < 3; j++) {
+//                     res.mat[i][j] = (res.mat[i][j] + r * other.mat[k][j]) % mod;
+//                 }
+//             }
+//         }
+//         return res;
+//     }
+// };
+
+// // 2. 矩阵快速幂函数：求 base 的 p 次方
+// Matrix qpow(Matrix base, int p) {
+//     Matrix res;
+//     // 初始化为单位矩阵 (对角线为1，相当于普通数字里的 1)
+//     res.mat[0][0] = res.mat[1][1] = res.mat[2][2] = 1; 
+    
+//     while (p > 0) {
+//         if (p & 1) res = res * base;
+//         base = base * base;
+//         p >>= 1;
+//     }
+//     return res;
+// }
+
+// void solve() {
+//     int n, m; cin >> n >> m;
+//     if (m == 1) {
+//         cout << 0 << '\n';
+//         return;
+//     }
+
+//     // 基础系数
+//     int c0 = (m - 1 + mod) % mod;
+//     int c1 = (m - 2 + mod) % mod;
+//     int c2 = (m - 3 + mod) % mod;
+
+//     // 3. 构建转移矩阵 M
+//     Matrix M;
+//     M.mat[0][0] = c0;
+//     M.mat[0][1] = 3 * c0 % mod * c1 % mod;
+//     M.mat[0][2] = c0 * c1 % mod * c2 % mod;
+
+//     M.mat[1][0] = c1;
+//     M.mat[1][1] = c1 * (3 * m % mod - 8 + mod) % mod;
+//     M.mat[1][2] = c1 * c2 % mod * c2 % mod;
+
+//     M.mat[2][0] = c2;
+//     M.mat[2][1] = 3 * c2 % mod * c2 % mod;
+//     M.mat[2][2] = (c1 * c1 % mod * c1 % mod - c2 * (3 * m % mod - 8 + mod) % mod + mod) % mod;
+
+//     // 4. 构建初始状态行向量 F1 (把它存为一个 1x3 的矩阵，我们放在 3x3 矩阵的第一行)
+//     Matrix F1;
+//     F1.mat[0][0] = m * c0 % mod;
+//     F1.mat[0][1] = m * 3 % mod * c0 % mod * c1 % mod;
+//     F1.mat[0][2] = m * c0 % mod * c1 % mod * c2 % mod;
+
+//     // 5. 核心：矩阵快速幂计算 M^(n-1)
+//     Matrix M_n_minus_1 = qpow(M, n - 1);
+
+//     // 计算 Fn = F1 * M^(n-1)
+//     Matrix Fn = F1 * M_n_minus_1;
+
+//     // 此时 Fn 的第一行就存放着 dp[n][0], dp[n][1], dp[n][2]
+//     int dp_n_0 = Fn.mat[0][0];
+//     int dp_n_1 = Fn.mat[0][1];
+//     int dp_n_2 = Fn.mat[0][2];
+
+//     // 6. 统计最终答案 (连接输出层)
+//     int ans = (dp_n_0 * c0 % mod + dp_n_1 * c1 % mod + dp_n_2 * c2 % mod) % mod;
+    
+//     cout << ans << '\n';
+// }
+
+// signed main() {
+//     ios::sync_with_stdio(false);
+//     cin.tie(0); cout.tie(0);
+//     int T = 1;
+//     // cin >> T;
+//     while(T--) solve();
+//     return 0;
+// }
